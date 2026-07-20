a09c553b85e62819a87db8735bec586be6fa537d
Sergoool <serzhkabulov@gmail.com>
2026-07-21 02:11:55 +0300

    Подключил БД

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,25 +1,40 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"task_tracker/internal/model"
 
 	"github.com/labstack/echo/v4"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"os"
 )
 
 
 func main() {
     e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		task := model.Task {
-			ID: "1",
-			Title: "Example",
-			Description: "work",
-			Status: model.New,
-			CreatedAt: "2026-07-20",
-			UpdatedAt: "2026-07-20",
-		}
-		return c.JSON(http.StatusOK, task)
+
+	dsn := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&model.Task{})
+
+	e.GET("/health", func(c echo.Context) error {
+		sqlDB, err := db.DB()
+		if err != nil || sqlDB.Ping() != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{
+			"status": "error",
+		})
+	}
+		return c.JSON(http.StatusOK, echo.Map{
+			"status": "everything is OK",
+	})
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
